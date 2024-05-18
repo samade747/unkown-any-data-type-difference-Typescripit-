08716bc726b77fcb0a52c6af44cad4eed305a770
samade747 <samad.e747@gmail.com>
2024-05-18 11:12:13 +0500

    1112am 18 5 2024

--- main.js
+++ main.js
@@ -5,4 +5,5 @@ function add(arg1, arg2) {
 console.log(add("1", "2")); // return type is number
 console.log(add(1, 2)); // return type is number
 console.log(add(true, false)); // return type is boolean
-console.log(add("22", 54)); // return type is boolean
+console.log(add("22", 54)); // return type is any
+console.log(add("22", 154)); // return type is any
--- main.ts
+++ main.ts
@@ -64,6 +64,7 @@
     console.log(add("1", "2")); // return type is number
     console.log(add(1, 2)); // return type is number
     console.log(add(true, false)); // return type is boolean
-    console.log(add("22", 54)); // return type is boolean
+    console.log(add("22", 54)); // return type is any
+    console.log(add("22", 154 )); // return type is any
 
 

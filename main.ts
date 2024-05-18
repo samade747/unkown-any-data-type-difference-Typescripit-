a4dbed35fe3aba8355118368868e8645c24cfad3
samade747 <samad.e747@gmail.com>
2024-05-18 10:03:31 +0500

    1003am 18 5 2024

--- main.ts
+++ main.ts
@@ -1,19 +1,29 @@
-// let num1: unknown = "18";
+// // let num1: unknown = "18";
 
-// let result: number = num1 as number;
-// // let result: number = <number>num1;
-// console.log(result);
-
-
-// type checking with type assertion\
-// if type checking 
-let num1: unknown = '18'; 
+// // let result: number = num1 as number;
+// // // let result: number = <number>num1;
+// // console.log(result);
 
 
+// // type checking with type assertion\
+// // if type checking 
+// let num1: unknown = '18'; 
 
-let result: number = num1 as number; // type assertion
-console.log(result); 
-// let result: number = <number>num1; // type assertion
+
+
+// let result: number = num1 as number; // type assertion
+// console.log(result); 
+// // let result: number = <number>num1; // type assertion
+
+
+// learning rest parameters
+function invite(guest: string) {
+    console.log("Hello " + guest);
+}
+
+
+invite("samad");
+
 
 
 

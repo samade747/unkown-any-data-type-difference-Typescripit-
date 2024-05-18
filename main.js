2dd558b4004443a2830c5e40b9f3052c0b33a4f3
samade747 <samad.e747@gmail.com>
2024-05-18 09:30:32 +0500

    930am 18 5 2024

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 var num1 = 18;
+// let result: number = num1 as number;
 var result = num1;
 console.log(result);
--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let num1: unknown = 18;
 
-let result: number = num1 as number;
+// let result: number = num1 as number;
+let result: number = <number>num1;
 console.log(result);
 
 

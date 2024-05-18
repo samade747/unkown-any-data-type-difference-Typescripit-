8d38c9b01bdde2ef96ffa76cac2e2477966961b7
samade747 <samad.e747@gmail.com>
2024-05-18 10:24:33 +0500

    1024am 18 5 2024

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
-// let num1: unknown = "18";
-// let result: number = num1 as number;
-// // let result: number = <number>num1;
-// console.log(result);
-// type checking with type assertion\
-// if type checking 
-var num1 = '18';
-var result = num1;
-console.log(result);
-// let result: number = <number>num1;
+// // let num1: unknown = "18";
+// // let result: number = num1 as number;
+// // // let result: number = <number>num1;
+// // console.log(result);
+// // type checking with type assertion\
+// // if type checking 
+// let num1: unknown = '18'; 
+// let result: number = num1 as number; // type assertion
+// console.log(result); 
+// // let result: number = <number>num1; // type assertion
+// learning rest parameters
+// function invite(host:string = "samad",...guest: string[] ) {
+//     console.log(guest);
+// }
+// invite("samad", "ali", "khan");
+function invite(host) {
+    var guest = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        guest[_i - 1] = arguments[_i];
+    }
+    console.log("$host");
+}
+invite("samad", "ali", "khan");
--- main.ts
+++ main.ts
@@ -17,13 +17,28 @@
 
 
 // learning rest parameters
-function invite(guest: string) {
-    console.log("Hello " + guest);
+// function invite(host:string = "samad",...guest: string[] ) {
+//     console.log(guest);
+// }
+
+
+// invite("samad", "ali", "khan");
+
+
+// // if we print the rest parameters only host will be printed
+// function invite(host:string, ...guest: string[] ) {
+//     console.log(`$host`);
+// }
+
+
+// invite("samad", "ali", "khan");
+
+// if we print the rest parameters only guest will be printed
+function invite(host:string, ...guest: string[] ) {
+    console.log(`$guest`);
 }
 
 
-invite("samad");
-
-
+invite("samad", "ali", "khan");
 
 
